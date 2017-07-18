d6274fdebe85f2abadfff14d23bf8200a8226183
Pablo Alonso <pabloalonsos.eu@gmail.com>
2017-07-18 08:18:15 -0400

    updates to Plan.sol

--- contracts/Plan.sol
+++ contracts/Plan.sol
@@ -20,25 +20,44 @@ contract Plan {
         _;
     }
 
+    modifier onlyMembers() {
+        require(membersMap[msg.sender]);
+        _;
+    }
+
     function Plan(bytes32 _name, address[] _members) {
         owner = tx.origin;
         name = _name;
         members = _members;
 
+        bool isOwnerAMember = false;
         for (uint i = 0; i < members.length; i++) {
+            if (members[i] == owner) {
+                isOwnerAMember = true;
+            }
             membersMap[members[i]] = true;
+        }
+
+        // add owner as member even if he hasn't been part of the initial members collection
+        if (!isOwnerAMember) {
+            members.push(owner);
+            membersMap[owner] = true;
         }
     }
 
     function transfer(address _newOwner) onlyOwner returns (bool) {
+        // check that the new owner is a member
+        require(membersMap[_newOwner]);
+
         owner = _newOwner;
         OwnershipTransferred(owner);
+
         return true;
     }
 
     function addMember(address newMember) onlyOwner returns (bool) {
-        // check if newMember is already a member
-        require(checkIfMember(newMember));
+        // make sure the new member is not an existing one
+        require(!membersMap[newMember]);
 
         // add member
         members.push(newMember);
@@ -54,15 +73,11 @@ contract Plan {
         return true;
     }
 
-    function addInitiative(uint256 votesNeededToPass, bytes32 name, bytes32 description) returns (Initiative newInitiativeAddress) {
+    function addInitiative(uint256 votesNeededToPass, bytes32 name, bytes32 description) onlyMembers returns (Initiative newInitiativeAddress) {
         Initiative newInitiative = new Initiative(members, votesNeededToPass, name, description);
         initiatives.push(newInitiative);
         NewInitiativeAdded(newInitiative);
         return newInitiative;
     }
 
-    function checkIfMember(address member) constant returns (bool) {
-        require(membersMap[member]);
-        return membersMap[member];
-    }
 }
